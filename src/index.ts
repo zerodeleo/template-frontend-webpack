132cdacb15a833bf8e7e61ed1d765d13becc7632
zerodeleo <julia.stjerna@hotmail.se>
2023-02-14 14:30:40 +0100

    WIP: TODO: add debug

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './styles/style.css';
 import * as THREE from 'three';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import gsap from 'gsap';
 
 /**
  * Canvas
@@ -15,32 +16,60 @@ export const canvas: HTMLCanvasElement = document.querySelector('#canvas')!;
 export const scene = new THREE.Scene();
 
 /**
- * Texture
+ * Textures
  */
 const textureLoader = new THREE.TextureLoader();
 const texture = textureLoader.load('/img/zerodeleo.png');
+const matcap = textureLoader.load('/textures/matcaps/matcap.jpeg');
+
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const environmentMapTexture = cubeTextureLoader.load([
+  '/textures/environmentMaps/0/px.png',
+  '/textures/environmentMaps/0/nx.png',
+  '/textures/environmentMaps/0/py.png',
+  '/textures/environmentMaps/0/ny.png',
+  '/textures/environmentMaps/0/pz.png',
+  '/textures/environmentMaps/0/nz.png'
+]);
 
 /**
- * Object
+ * Objects
  */
-const cube = new THREE.BoxGeometry(4, 4);
+const sphere = new THREE.SphereGeometry(0.8);
 const material = new THREE.MeshBasicMaterial({ map: texture });
-const mesh = new THREE.Mesh(cube, material);
-scene.add(mesh);
+const sphereMesh = new THREE.Mesh(sphere, material);
+sphereMesh.position.set(0, -10, -10);
+scene.add(sphereMesh);
+
+const cube = new THREE.BoxGeometry(3, 3, 3);
+const env = new THREE.MeshStandardMaterial();
+env.metalness = 1;
+env.roughness = 0;
+env.envMap = environmentMapTexture;
+const cubeMesh = new THREE.Mesh(cube, env);
+cubeMesh.position.set(0, 0, -5);
+scene.add(cubeMesh);
 
 /**
  * Text
  */
 const fontStyle = {
-  size: 0.05,
-  height: 0.001
+  size: 0.8,
+  height: 0.05,
+  curveSegments: 12,
+  bevelEnabled: true,
+  bevelThickness: 0.03,
+  bevelSize: 0.02,
+  bevelOffset: 0,
+  bevelSegments: 5
 };
 const fontLoader = new FontLoader();
-fontLoader.load('/fonts/', (font) => {
-  const txtMaterial = new THREE.MeshBasicMaterial();
+fontLoader.load('/fonts/optimer_regular.typeface.json', (font) => {
+  const txtMaterial = new THREE.MeshMatcapMaterial({ matcap });
   const txtGeometry = new TextGeometry('zerodeleo', { font, ...fontStyle });
   const txt = new THREE.Mesh(txtGeometry, txtMaterial);
-  txt.position.set(0, 5, 1);
+  txt.position.set(0, -10, -5);
+  txtGeometry.center();
   scene.add(txt);
 });
 
@@ -71,7 +100,29 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 /**
  * Animation
  */
+const duration = 1;
+const delay = 0;
+const SPEED = 0.3;
+
+gsap.to(sphereMesh.rotation, {
+  duration,
+  delay,
+  x: 1.9 * Math.PI,
+  y: 1.5 * Math.PI
+});
+
+const clock = new THREE.Clock();
+
 const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update Object
+  if (elapsedTime > duration + delay) {
+    cubeMesh.rotation.x = (elapsedTime - (duration + delay)) * SPEED;
+    cubeMesh.rotation.y = (elapsedTime - (duration + delay)) * SPEED;
+  }
+  camera.lookAt(cubeMesh.position);
+
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
