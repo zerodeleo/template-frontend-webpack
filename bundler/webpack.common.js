ed0781b348ba07b193a5babba21c6886d96ca451
zerodeleo <77724831+zerodeleo@users.noreply.github.com>
2023-03-16 15:55:05 +0100

    Update webpack.common.js

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -5,6 +5,9 @@ const path = require('path')
 
 module.exports = {
     entry: path.resolve(__dirname, '../src/index.js'),
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
     output:
     {
         hashFunction: 'xxhash64',
